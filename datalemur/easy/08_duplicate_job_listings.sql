30bbea99b477e8a782a171d7a5dd038b747a25c8
Omar Elhadidi <omar.t.elhadidi@gmail.com>
2026-09-14 22:02:53 +0300

    feat: add solution for 08_duplicate_job_listings (LinkedIn)

---
+++ datalemur/easy/08_duplicate_job_listings.sql
@@ -0,0 +1,12 @@
+﻿-- Problem: Duplicate Job Listings
+-- Platform: DataLemur (Easy)
+-- URL: https://datalemur.com/questions/duplicate-job-listings
+
+WITH duplicate_listings AS (
+  SELECT company_id, title, description, COUNT(job_id) 
+  FROM job_listings
+  GROUP BY company_id, title, description
+  HAVING COUNT(job_id) > 1
+)
+SELECT COUNT(*) AS duplicate_companies
+FROM duplicate_listings;
