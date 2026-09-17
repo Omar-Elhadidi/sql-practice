1fb9d3a8ac986099cf82dc9b2c71b6b38e8aa895
Omar Elhadidi <omar.t.elhadidi@gmail.com>
2026-09-18 02:49:14 +0300

    feat: add solution for 15_second_day_confirmation (TikTok)

---
+++ datalemur/easy/15_second_day_confirmation.sql
@@ -0,0 +1,10 @@
+﻿-- Problem: Second Day Confirmation
+-- Platform: DataLemur (Easy)
+-- URL: https://datalemur.com/questions/second-day-confirmation
+
+SELECT e.user_id
+FROM emails e
+INNER JOIN texts t
+  ON e.email_id = t.email_id
+WHERE DATE(e.signup_date) + 1 = DATE(t.action_date) 
+  AND t.signup_action = 'Confirmed';
