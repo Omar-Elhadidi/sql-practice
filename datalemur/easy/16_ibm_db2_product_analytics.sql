82df9dc887cccab1002b4cef5fe059bd423b499b
Omar Elhadidi <omar.t.elhadidi@gmail.com>
2026-09-19 03:15:43 +0300

    feat: add solution for 16_ibm_db2_product_analytics (IBM)

---
+++ datalemur/easy/16_ibm_db2_product_analytics.sql
@@ -0,0 +1,20 @@
+﻿-- Problem: IBM Db2 Product Analytics
+-- Platform: DataLemur (Easy)
+-- URL: https://datalemur.com/questions/ibm-db2-product-analytics
+
+WITH queries_filtered AS (
+  SELECT * 
+  FROM queries  
+  WHERE query_starttime >= '2023-07-01' AND query_starttime < '2023-10-01'
+), 
+queries_count AS (  
+  SELECT e.employee_id, COUNT(q.query_id) AS unique_queries
+  FROM employees e
+  LEFT JOIN queries_filtered q
+    ON e.employee_id = q.employee_id
+  GROUP BY e.employee_id
+)
+SELECT unique_queries, COUNT(employee_id) AS employee_count
+FROM queries_count
+GROUP BY unique_queries
+ORDER BY unique_queries;
